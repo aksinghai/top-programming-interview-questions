de7609fc468bb072723018f60fa6ad22e4d15091
“Akshay <aksinghai@outlook.com>
2020-12-09 12:07:51 +0530

    added new recursion approach

--- src/com/company/Arrays/A1_ReverseTheArray.java
+++ src/com/company/Arrays/A1_ReverseTheArray.java
@@ -6,11 +6,13 @@ public class A1_ReverseTheArray {
     public static void main(String[] args) {
         int[] arr = {4, 5, 1, 2};
         System.out.println(Arrays.toString(arr));
-        reverseAnArray(arr);
+        //reverseAnArrayApproach1(arr);
+        reverseAnArrayApproach2(arr, 0, arr.length-1);
         System.out.println(Arrays.toString(arr));
     }
 
-    private static void reverseAnArray(int[] arr){
+    //Using Loop O(n)
+    private static void reverseAnArrayApproach1(int[] arr){
         int i = 0;
         int j = arr.length - 1;
         while (i < j){
@@ -19,6 +21,16 @@ public class A1_ReverseTheArray {
             arr[j] = t;
             i++;
             j--;
+        }
+    }
+
+    //Using Recursive O(n)
+    private static void reverseAnArrayApproach2(int[] arr, int i, int j){
+        if (i < j){
+            int t = arr[i];
+            arr[i] = arr[j];
+            arr[j] = t;
+            reverseAnArrayApproach2(arr, i+1, j-1);
         }
     }
 }
